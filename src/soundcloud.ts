19acecd13801afb841b1aa52d0e59416497665dc
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-03-06 10:28:42 -0500

    Log event and context

--- src/soundcloud.ts
+++ src/soundcloud.ts
@@ -1,9 +1,12 @@
 export const handler = async (event: any, context: any) => {
-  const queryString = event.queryStringParameters;
-  const query = JSON.stringify(queryString);
+  // const queryString = event.queryStringParameters;
+  // const query = JSON.stringify(queryString);
+  const strEvent = JSON.stringify(event);
+  const strContext = JSON.stringify(context);
 
   return {
     statusCode: 200,
-    body: `Hello world!\nquery is ${query}`,
+    // body: `Hello world!\nquery is ${query}`,
+    body: `${strEvent} ${strContext}`,
   };
 };
