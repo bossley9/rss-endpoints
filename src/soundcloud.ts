99e894e412a146b46814275429a36a559d5655ea
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-03-06 00:03:51 -0500

    Switch to native TS and functions

---
+++ src/soundcloud.ts
@@ -0,0 +1,6 @@
+export const handler = async (event: any, content: any) => {
+  return {
+    statusCode: 200,
+    body: "Hello world!",
+  };
+};
---
+++ test/soundcloud.test.js
@@ -0,0 +1,5 @@
+// const soundcloud = require("../src/soundcloud");
+
+test("it works", () => {
+  expect(3).toEqual(3);
+});
