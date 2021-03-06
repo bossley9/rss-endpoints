87502596408a05c37ffc249c44dfe8f588bab7d4
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-03-06 10:20:11 -0500

    Print query string parameters

--- src/soundcloud.ts
+++ src/soundcloud.ts
@@ -1,6 +1,9 @@
-export const handler = async (event: any, content: any) => {
+export const handler = async (event: any, context: any) => {
+  const queryString = event.queryStringParameters;
+  const query = JSON.Stringify(queryString);
+
   return {
     statusCode: 200,
-    body: "Hello world!",
+    body: `Hello world!\nquery is ${query}`,
   };
 };
