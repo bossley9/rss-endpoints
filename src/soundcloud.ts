e3cae0a5e7e166dfd2cb62791d202b45bb9576c7
Sam Bossley <35381535+bossley9@users.noreply.github.com>
2021-03-08 18:21:21 -0500

    Test soundcloud user tracks

--- src/soundcloud.ts
+++ src/soundcloud.ts
@@ -1,11 +1,45 @@
-export const handler = async (event: any) => {
-  const path = event.path;
-  const queryString = event.queryStringParameters;
+import fetch from "isomorphic-fetch";
 
-  const query = JSON.stringify(queryString);
+export const handler = async (event: any) => {
+  const { path } = event;
+  let statusCode = 200;
+  let body = "";
+
+  const pathIndicator: string = "soundcloud";
+  const indicatorIndex = path.indexOf(pathIndicator);
+  const indicatorLength = pathIndicator.length;
+
+  const user = path.substring(indicatorIndex + indicatorLength + 1);
+
+  let userid = "";
+
+  try {
+    const url = `http://soundcloud.com/${user}`;
+    const res = await fetch(url, {});
+    const raw = await res.text();
+
+    const key: string = "api.soundcloud.com/users/";
+
+    // trim start and end to get id
+    userid = raw;
+    userid = userid.substring(userid.indexOf(key) + key.length);
+    userid = userid.substring(0, userid.indexOf('"'));
+  } catch (e) {
+    console.log("Error:", e);
+    statusCode = 500;
+  }
+
+  try {
+    const url = `https://feeds.soundcloud.com/users/soundcloud:users:${userid}/sounds.rss`;
+    const res = await fetch(url, {});
+    body = await res.text();
+  } catch (e) {
+    console.log("Error:", e);
+    statusCode = 500;
+  }
 
   return {
-    statusCode: 200,
-    body: `Hello world!\npath is ${path}\nquery is ${query}`,
+    statusCode,
+    body,
   };
 };
--- test/soundcloud.test.js
+++ test/soundcloud.test.js
@@ -1,5 +1,17 @@
-// const soundcloud = require("../src/soundcloud");
+const soundcloud = require("../src/soundcloud");
+const { handler } = soundcloud;
 
-test("it works", () => {
-  expect(3).toEqual(3);
+describe("soundcloud", () => {
+  // routine
+  it("produces the expected output format", async () => {
+    const event = {
+      path: "/.netlify/functions/soundcloud",
+      queryStringParameters: {},
+    };
+
+    const res = await handler(event);
+
+    expect(res.statusCode).toEqual(200);
+    expect(typeof res.body).toBe("string");
+  });
 });
